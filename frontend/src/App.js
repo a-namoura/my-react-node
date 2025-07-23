54a28acb5a839b2c3f1b4546d3a0920b1a4143f6
namoura <130542021+a-namoura@users.noreply.github.com>
2025-07-23 11:47:34 +0200

    Added texts and buttons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <div>
+      <h1>Hello World</h1>
+      <h2>Hello World</h2>
+      <h3>Hello World</h3>
+      <p>This is a paragraph</p>
+      <button onClick={() => alert('Hello World!')}>Click Me</button>
+    </div>
   );
 }
 
